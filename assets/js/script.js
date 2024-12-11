a1513de56d398b3ecd1b23219c367451899187cf
camillazicari <camyzicari01@gmail.com>
2024-12-11 16:42:29 +0100

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
 const card = document.querySelectorAll('.card');
+const url = 'https://striveschool-api.herokuapp.com/books';
+let myBooks;
+
+document.addEventListener('load', init());
+
+function init() {
+    getData()
+}
+
+function getData() {
+    fetch(url).then((response) => {
+        return response.json();
+    }).then((data) => {
+        myBooks = data;
+        printBooks();
+    }).catch((error) => {
+        console.log('errore nel recupero dei libri: ', error);
+    });
+}
+
+
+function printBooks() {
+    for (let i=0; i<card.length; i++) {
+        let image = card[i].querySelector('img');
+        image.setAttribute('src', myBooks[i].img);
+        let price = card[i].querySelector('.card-price');
+        price.innerText = `${myBooks[i].price} €`
+    }
+}
+
+
